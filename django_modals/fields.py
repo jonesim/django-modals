6676e5b880010f34e1fc15bec77e8e3eda2fe13d
Ian Jones <ian@cobi.co.uk>
2021-06-23 10:04:40 +0100

    Add crispy field options

--- django_modals/fields.py
+++ django_modals/fields.py
@@ -1,7 +1,7 @@
 from django.forms import ChoiceField, MultipleChoiceField
 from crispy_forms.layout import Field, HTML, Div
 from crispy_forms.utils import TEMPLATE_PACK
-
+from crispy_forms.bootstrap import PrependedText, AppendedText, PrependedAppendedText
 
 date_picker = {'autocomplete': 'off', 'css_class': 'datepicker', 'style': 'width:110px'}
 
@@ -14,7 +14,7 @@ def html_label(title):
     return Div(Div(HTML(title), css_class='col-form-label form-control-sm'), css_class='col-md-3')
 
 
-def toggle(field, options, **kwargs):
+def toggle(field, options, **_kwargs):
     return Field(field, data_toggle="toggle", data_size="small", data_on=options[0], data_off=options[1])
 
 
@@ -30,58 +30,62 @@ class FieldToggle(Field):
 
 
 def reverse_field(*args, field_class='form-group-sm', **kwargs):
-    return FieldBootStrap(*args, css_class='form-control-sm', field_class=field_class, group_class='d-flex',
-                          label_class='form-control-sm', **kwargs, template='modal_fields/reverse.html')
+    return FieldOptions(*args, css_class='form-control-sm', field_class=field_class, group_class='d-flex',
+                        label_class='form-control-sm', **kwargs, template='modal_fields/reverse.html')
 
 
 def multi_row(label, row):
     return Div(
         html_label(label),
-        Div(Div(*row, css_class='d-flex'), css_class='col-md-9 col-lg-6'),
+        Div(Div(*row, css_class='d-flex')),
         css_class="row form-group multi-field")
 
 
-class FieldBootStrap(Field):
+class AddFieldClassOptionsMixin:
 
     def __init__(self, *args, **kwargs):
-        self.label_class = kwargs.pop('label_class', None)
-        self.field_class = kwargs.pop('field_class', None)
-        self.group_class = kwargs.pop('group_class', None)
-
+        self.extra_classes = {c: kwargs.pop(c) for c in ['label_class', 'field_class', 'form_class'] if c in kwargs}
         super().__init__(*args, **kwargs)
 
     def render(self, form, form_style, context, template_pack=TEMPLATE_PACK, extra_context=None, **kwargs):
         if extra_context is None:
             extra_context = {}
-        if self.label_class is not None:
-            extra_context['label_class'] = self.label_class
-        if self.field_class is not None:
-            extra_context['field_class'] = self.field_class
-        if self.group_class:
-            extra_context['group_class'] = self.group_class
+        extra_context.update(self.extra_classes)
+        # noinspection PyUnresolvedReferences
         return super().render(form, form_style, context, template_pack, extra_context, **kwargs)
 
 
-class MultiField(FieldBootStrap):
+class FieldOptions(AddFieldClassOptionsMixin, Field):
+    pass
 
+
+class PrependedTextOptions(AddFieldClassOptionsMixin, PrependedText):
+    pass
+
+
+class AppendedTextOptionsMixin(AddFieldClassOptionsMixin, AppendedText):
+    pass
+
+
+class PrependedAppendedTextOptions(AddFieldClassOptionsMixin, PrependedAppendedText):
+    pass
+
+
+class MultiField(FieldOptions):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.label_class = kwargs.pop('label_class', 'px-1')
-        self.field_class = kwargs.pop('field_class', 'input-group-sm')
-        self.group_class = kwargs.pop('group_class', 'form-group mb-0')
-
-
-class FieldNoLabel(FieldBootStrap):
-    def __init__(self, *args, **kwargs):
-        if 'label_class' not in kwargs:
-            kwargs['label_class'] = 'd-none'
-        if 'field_class' not in kwargs:
-            kwargs['field_class'] = 'input-group-sm'
-        if 'group_class' not in kwargs:
-            kwargs['group_class'] = 'dummy'
+        kwargs.setdefault('label_class', 'col-form-label-sm')
+        kwargs.setdefault('field_class', 'input-group-sm mr-2')
+        kwargs.setdefault('wrapper_class', 'mb-0')
         if 'width' in kwargs:
-            kwargs['style'] = "width:" + str(kwargs['width']) + "px"
-        super(FieldNoLabel, self).__init__(*args, **kwargs)
+            kwargs['style'] = f'width:{kwargs.pop("width")}px'
+        super().__init__(*args, **kwargs)
+
+
+class FieldNoLabel(MultiField):
+    def __init__(self, *args, **kwargs):
+        kwargs['label_class'] = 'd-none'
+        kwargs['form_class'] = ''
+        super().__init__(*args, **kwargs)
 
 
 class MultipleChoiceFieldExtra(MultipleChoiceField):
