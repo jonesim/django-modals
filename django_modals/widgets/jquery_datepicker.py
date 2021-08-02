dba5f3bef3fc4d9f80d78d4517c97d33da958029
jonesim <ian@cobi.co.uk>
2021-08-02 18:08:37 +0100

    Correct renamed FieldOptions

--- django_modals/widgets/jquery_datepicker.py
+++ django_modals/widgets/jquery_datepicker.py
@@ -1,8 +1,8 @@
 from django.forms import DateInput
-from ..fields import FieldOptions
+from ..fields import FieldEx
 
 
 class DatePicker(DateInput):
     template_name = 'django_modals/widgets/datepicker.html'
-    crispy_field_class = FieldOptions
+    crispy_field_class = FieldEx
     crispy_kwargs = {'appended_text': '<i class="fas fa-calendar-alt fa-fw"></i>', 'input_size': 'input-group-sm'}
