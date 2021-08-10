f6335facc122afd31c5a48d52b497f7a90d513f9
jonesim <ian@cobi.co.uk>
2021-08-10 10:16:48 +0100

    Refine datatable columns

--- django_modals/datatables.py
+++ django_modals/datatables.py
@@ -31,21 +31,27 @@ class ModalLink(ColumnBase):
 
 
 class EditColumn(ModalLink):
-    def __init__(self, modal_name, button_text='Edit', css_class='btn btn-sm btn-primary', title='Edit',
+    def __init__(self, modal_name, button_text='Edit', css_class='btn btn-sm btn-primary', title='',
                  no_col_search=True, **kwargs):
         super().__init__(modal_name=modal_name, button_text=button_text, title=title, no_col_search=no_col_search,
                          css_class=css_class, **kwargs)
+        self.column_defs = {
+            'orderable': False,
+            'className': 'dt-center'
+        }
 
 
 class DeleteColumn(ColumnBase):
 
-    def __init__(self, modal_name,  field='id', button_text=None, css_class=None, no_col_search=True, **kwargs):
+    def __init__(self, modal_name,  field='id', button_text=None, css_class=None, no_col_search=True, title='',
+                 **kwargs):
         if not self.initialise(locals()):
             return
         super().__init__(**self.kwargs)
-        self.title = 'Delete'
         css_class = css_class if css_class else 'text-danger'
         button_text = button_text if button_text else modal_buttons['delete']
+        self.column_defs['orderable'] = False
+        self.column_defs['className'] = 'dt-center'
         self.options['render'] = [render_replace(
             var='999999',
             html=f'<a class="{css_class}" href="javascript:{modal_delete_javascript(url_name=modal_name, pk=999999)}">'
