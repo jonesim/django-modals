c7caefa413af69202542ff26d530c733ca4b29fe
jonesim <ian@cobi.co.uk>
2021-08-19 15:09:58 +0100

    Remove show_src as external package now

--- django_examples/show_src_code/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .modals import BaseSourceCodeModal
-
-app_name = 'show_src_code'
-urlpatterns = [
-    path('modal/source_code/<str:slug>/', BaseSourceCodeModal.as_view(), name='source_code_modal'),
-]
