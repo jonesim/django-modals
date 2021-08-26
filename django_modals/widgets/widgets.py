21de6876b97995efa1a1788a77e91ac6eaa5d28d
jonesim <ian@cobi.co.uk>
2021-08-26 16:30:23 +0100

    PEP8

--- django_modals/widgets/widgets.py
+++ django_modals/widgets/widgets.py
@@ -3,10 +3,9 @@ from django.forms import CheckboxInput, Textarea
 
 class Toggle(CheckboxInput):
     template_name = 'django_modals/widgets/toggle.html'
-    crispy_kwargs = {'template': 'django_modals/fields/label_checkbox.html', 'field_class':'col-6 input-group-sm'}
+    crispy_kwargs = {'template': 'django_modals/fields/label_checkbox.html', 'field_class': 'col-6 input-group-sm'}
 
 
 class TinyMCE(Textarea):
     template_name = 'django_modals/widgets/tinymce.html'
-    crispy_kwargs = {'label_class':'col-3 col-form-label-sm', 'field_class':'col-12 input-group-sm'}
-
+    crispy_kwargs = {'label_class': 'col-3 col-form-label-sm', 'field_class': 'col-12 input-group-sm'}
