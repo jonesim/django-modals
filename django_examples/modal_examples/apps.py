edaf758669739d89e63b72a396f376772ebdc610
jonesim <ian@cobi.co.uk>
2021-09-20 11:09:50 +0100

    Auto import initial data and change management command name

--- django_examples/modal_examples/apps.py
+++ django_examples/modal_examples/apps.py
@@ -6,3 +6,7 @@ class ModalExampleConfig(PypiAppConfig):
     name = 'modal_examples'
     pypi = 'django-nested-modals'
     urls = 'modal_examples.urls'
+
+    def ready(self):
+        from .import_data import import_data
+        import_data(self.path)
--- django_examples/modal_examples/management/commands/import_modal_data.py
+++ django_examples/modal_examples/management/commands/import_modal_data.py
@@ -1,32 +1,8 @@
-import csv
-import datetime
+from django.apps import apps
 from django.core.management.base import BaseCommand
-from django.conf import settings
-from modal_examples import models
+from modal_examples.import_data import import_data
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-
-        with open(str(settings.BASE_DIR) + '/test_data.csv', 'r') as f:
-
-            titles = {c[1]:c[0] for c in models.Person.title_choices}
-            csv_reader = csv.DictReader(f)
-            for r in csv_reader:
-
-                companies = models.Company.objects.filter(name=r['Company'])
-                if len(companies) > 1:
-                    for c in companies[1:]:
-                        c.delete()
-
-                company = models.Company.objects.get_or_create(name=r['Company'])[0]
-                models.Person.objects.get_or_create(company=company,
-                                                    first_name=r['First Name'],
-                                                    surname=r['Surname'],
-                                                    title=titles.get(r['Title'], None),
-                                                    date_entered=datetime.datetime.strptime(r['date_entered'],
-                                                                                            '%d/%m/%Y'))
-                for tag in r['tags'].split(','):
-                    if tag!='':
-                        tag = models.Tags.objects.get_or_create(tag=tag)[0]
-                        tag.company.add(company)
+        import_data(apps.get_app_config('modal_examples').path)
