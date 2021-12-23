57f519a0e0ed086a90f1f71acdd4ef54f69ed99a
jonesim <ian@cobi.co.uk>
2021-12-23 14:35:08 +0000

    Update scratchpad for ajax_helper changes

--- django_modals/mixins.py
+++ django_modals/mixins.py
@@ -3,10 +3,14 @@ from django.template.loader import render_to_string
 
 class ScratchPad:
 
-    def button_scratchpad(self, **_kwargs):
-        return self.command_response(
-            'append_to', html=render_to_string('django_modals/scratchpad.html'), check_id='scratchpad', element='body'
-        )
+    def button_scratchpad(self, *, scratchpad):
+        if scratchpad == 0:
+            return self.command_response(
+                'append_to', html=render_to_string('django_modals/scratchpad.html'),
+                check_id='scratchpad', selector='body'
+            )
+        else:
+            return self.command_response('null')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
