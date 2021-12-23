49711259afa172a2806690fd231b27e78f4a619b
jonesim <ian@cobi.co.uk>
2021-12-23 14:50:16 +0000

    Make ModalAjaxFileMixin use AjaxFileUploadMixin from ajax_helpers

--- django_modals/modal_upload.py
+++ django_modals/modal_upload.py
@@ -1,8 +1,10 @@
-import json
+from ajax_helpers.mixins import AjaxFileUploadMixin
+
 from django_modals.modals import Modal
 from django_modals.helper import ajax_modal_replace
 from .helper import progress_bar
 from .messages import AjaxMessagesMixin
+
 
 class UploadModal(Modal):
 
@@ -17,50 +19,19 @@ class UploadModal(Modal):
         return ''.join(progress_bars)
 
 
-class ModalAjaxFileMixin(AjaxMessagesMixin):
-    ajax_commands = ['start_upload', 'upload']
+class ModalAjaxFileMixin(AjaxMessagesMixin, AjaxFileUploadMixin):
+    single_progress_bar = False
 
     def upload_completed(self):
         self.add_command('html', selector='#upload_message', html='Upload Complete')
         self.add_command('delay', time=1800)
         return self.command_response('reload')
 
-    def post(self, request, *args, **kwargs):
-        if request.is_ajax() and request.content_type == 'multipart/form-data' and 'ajax_modal_file' in request.FILES:
-            response = request.POST.dict()
-            file = self.request.FILES['ajax_modal_file']
-            upload_params = json.loads(response.get('upload_params', '{}'))
-            index = int(response.pop('index'))
-            file_info = [f for f in json.loads(response['file_info']) if f['size'] > 0]
-            getattr(self, 'upload_' + request.POST['upload'])(file_info[index]['name'], file_info[index]['size'], file,
-                                                              **upload_params)
-            if len(file_info) > index + 1:
-                response['upload_params'] = upload_params
-                self.response_commands.append(self.upload_file_command(index + 1, **response))
-                return self.command_response()
-            return self.upload_completed()
-        return super().post(request, *args, **kwargs)
-
     def start_upload_files(self, **kwargs):
-        files = [f for f in kwargs['files'] if f['size'] > 0]
-        if len(files) == 0:
+        kwargs['files'] = [f for f in kwargs['files'] if f['size'] > 0]
+        if len(kwargs['files']) == 0:
             return self.error_message('No files selected')
         self.response_commands.append(
-            ajax_modal_replace(self.request, modal_class=UploadModal, ajax_function='modal_html',
-                               files=files))
-        kwargs['files'] = files
-        self.response_commands.append(self.upload_file_command(0, **kwargs))
-        return self.command_response()
-
-    @staticmethod
-    def upload_file_command(index, **kwargs):
-        upload_file = {'options': {'progress': {'selector': f'#file_progress_bar_{index}'}},
-                       'index': index,
-                       'function': 'upload_file'}
-        if 'upload_params' in kwargs:
-            upload_file['upload_params'] = kwargs['upload_params']
-        if 'drag_drop' in kwargs:
-            upload_file['drag_drop'] = kwargs['drag_drop']
-        else:
-            upload_file['selector'] = kwargs['selector']
-        return upload_file
+            ajax_modal_replace(self.request, modal_class=UploadModal, ajax_function='modal_html', files=kwargs['files'])
+        )
+        return self.command_response('upload_file', **self.upload_file_command(0, **kwargs))
