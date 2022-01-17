01f266accfc92296fde432cae6eb0a883afb4830
jonesim <ian@cobi.co.uk>
2022-01-17 13:42:12 +0000

    Up version to 0.0.18

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="django-nested-modals",
-    version="0.0.17",
+    version="0.0.18",
     author="Ian Jones",
     description="Django app to implement Bootstrap nested modals",
     long_description=long_description,
